17f75c88cc45965c5649eb0200ef4bd47f0c5b5e
Yang Hu <hyytez@gmail.com>
2019-02-13 09:31:44 +0800

    finished backend coding

---
+++ app.js
@@ -0,0 +1,103 @@
+// jshint esversion:6
+
+// requires
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+let playerScore = 0;
+
+// express setup
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+// mongodb setup
+mongoose.connect("mongodb://localhost:27017/simonDB", {
+  useNewUrlParser: true
+});
+
+// schema
+const playerSchema = new mongoose.Schema({
+  playerName: String,
+  playerScore: Number
+});
+
+// collection model
+const Player = mongoose.model('Player', playerSchema);
+
+
+//routes
+app.get("/", function(req, res) {
+  playerScore = 0;
+  res.render('index');
+});
+
+
+app.post("/result", function(req, res) {
+  console.log("received POST request on /results");
+  playerScore = req.body.score;
+  res.render("result", {
+    playerScore: playerScore
+  });
+});
+
+
+
+
+app.get("/leaderboard/:playerId", function(req, res) {
+  console.log("received get request on /leaderboard/playerId");
+  const playerId = req.params.playerId;
+  let playerRank;
+  let player;
+
+  Player.findOne({_id: playerId}, function(err, foundPlayer){
+    player = foundPlayer;
+
+    Player.find({}).sort({
+      playerScore: "desc",
+    }).exec(function(err, foundPlayers) {
+      if (foundPlayers) {
+        playerRank = foundPlayers.findIndex(x => x._id.toString() === playerId.toString()) + 1;
+      }
+
+      Player.find({}).sort({"playerScore": "desc"}).limit(10).exec(function(err, docs){
+        if (docs) {
+          res.render("leaderboard", {
+            foundPlayers: docs,
+            playerRank: playerRank,
+            playerName: player.playerName,
+            playerScore: player.playerScore
+          });
+        }
+      });
+    });
+  });
+
+
+
+
+});
+
+app.post("/leaderboard", function(req, res) {
+  console.log("received POST request on /leaderboard");
+  const newRecord = new Player({
+    playerName: req.body.playerName,
+    playerScore: playerScore
+  });
+  newRecord.save();
+  setTimeout(function(){
+    res.redirect(`/leaderboard/${newRecord._id.toString()}`);
+  }, 500);
+
+});
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000!");
+});
---
+++ public/js/game.js
@@ -0,0 +1,90 @@
+// jshint esversion:6
+
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+function playSound(name) {
+  let sound = new Audio(`sounds/${name}.mp3`);
+  sound.play();
+}
+
+function nextSequence() {
+  // console.log('next');
+  userClickedPattern = [];
+  // console.log(userClickedPattern);
+  level++;
+  $('#level-title').text(`LEVEL ${level}`);
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  // console.log(gamePattern);
+}
+
+function animatePress(currentColor) {
+  $(`.${currentColor}`).addClass('pressed');
+  setTimeout(function() {
+    $(`.${currentColor}`).removeClass('pressed');
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    // console.log('success');
+    // check if user has finished the sequence
+    if (userClickedPattern.length === level) {
+      $('.game-btn').off('click');
+      setTimeout(function() {
+        nextSequence();
+        enableButtonClick();
+      }, 1000);
+    }
+  } else {
+    // console.log('fail');
+    $('.game-btn').off('click');
+    $('.playerScore').attr('value', level);
+    playSound('wrong');
+    $('body').addClass('game-over');
+    setTimeout(function() {
+      $('body').removeClass('game-over');
+    }, 200);
+    setTimeout(function() {
+      $('.submitScore').submit();
+    }, 500);
+    // $('#level-title').text('GAME OVER! Press Any Key to restart');
+
+
+    // startOver();
+  }
+}
+
+function startOver() {
+  $('.game-btn').off('click');
+  gamePattern = [];
+  level = 0;
+  started = false;
+}
+
+function enableButtonClick() {
+  $('.game-btn').on('click', function(e) {
+    let userChosenColor = $(e.target).attr('id');
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+  });
+}
+
+
+$(document).on('keydown', function(e) {
+  if (started === false) {
+    enableButtonClick();
+    nextSequence();
+    started = true;
+    // console.log(e.key);
+  }
+});
